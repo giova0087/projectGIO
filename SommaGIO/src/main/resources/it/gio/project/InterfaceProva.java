b96a6272f2a39ce243f0ad00bada347f72ef6440
giova0087 <giovanni_colicci@msn>
2018-04-06 23:19:36 +0200

    init

---
+++ SommaGIO/src/main/resources/it/gio/project/InterfaceProva.java
@@ -0,0 +1,7 @@
+package it.gio.project;
+
+public interface interfacdeGIO {
+	
+	public int somma (int a , int b);
+
+}
---
+++ SommaGIO/src/main/resources/it/gio/project/MyClass.java
@@ -0,0 +1,25 @@
+package it.gio.project;
+
+import it.gio.project.interfacdeGIO;
+
+public class MyClass implements interfaceProva{
+
+	public MyClass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args){
+		
+		
+		int a = 0;
+		
+		a++;
+		
+		int b =0;
+		
+		int s = a+b;
+		
+		System.out.println("Prova!"+s);
+	}
+
+}
